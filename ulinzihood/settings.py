6f074001253827b7a2111dfab0f42307b99f535d
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2022-01-10 05:32:19 +0300

    installed cloudonary storage

--- ulinzihood/settings.py
+++ ulinzihood/settings.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'cloudinary_storage',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
